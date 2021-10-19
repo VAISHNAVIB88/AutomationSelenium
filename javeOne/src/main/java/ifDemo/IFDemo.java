9c6a4acff965d1094b11b1c68ed7f45e46b228b0
Vaishnavi Babu <vaishnavi.b88@gmail.com>
2021-10-19 19:11:43 +1300

    Second commit

--- javeOne/src/main/java/ifDemo/IFDemo.java
+++ javeOne/src/main/java/ifDemo/IFDemo.java
@@ -1,24 +1,18 @@
 package ifDemo;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class IFDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num = 21;
 
-		if (num%3 == 0 && num%5 == 0) {
-			System.out.println("TRIZZ-FIZZ");
-		} 
-		else if (num%3 == 0) {
+		if (num % 3 == 0 && num % 5 == 0) {
+			System.out.println("TRIZZ-FIZZZ");
+		} else if (num % 3 == 0) {
 			System.out.println("TRIZZ");
-		}
-		else if (num%5 == 0) {
+		} else if (num % 5 == 0) {
 			System.out.println("FIZZ");
 		}
-		
-		
-}
+
+	}
 }
